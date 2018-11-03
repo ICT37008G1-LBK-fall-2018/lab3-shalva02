3c55a504e9e2c11d3fc9d020e7bdbf96e47a0b5c
mi5hiko <mishikorekhviashvili0@gmail.com>
2018-11-03 11:05:11 +0400

    lab3

--- L301/script.js
+++ L301/script.js
@@ -1,7 +1,8 @@
 var text = "რეკლამა მარკეტინგი ვირუსი";
-if(text.indexOf("რეკლამა") >= 0){
+var words = [ "რეკლამა", "მარკეტინგი", "ვირუსი" ];
+if(text.indexOf(words[0]) >= 0 && text.indexOf(words[1]) >= 0 && text.indexOf(words[2]) >= 0){
     alert("სტრიქონი შეიცავს აკრძალულ სიტყვებს");
-}else if(text.indexOf("რეკლამა") < 0){
+}else if(text.indexOf(words[0]) < 0 && text.indexOf(words[1]) < 0 && text.indexOf(words[2]) < 0){
     alert("სტრიქონი არ შეიცავს აკრძალულ სიტყვებს");
 
 }
