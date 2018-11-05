646ef6450da39afea3b6805e86a274d3d68e60d5
shalva02 <zaqaidzes@gmail.com>
2018-11-05 13:02:06 +0400

    lab3

--- L305/script.js
+++ L305/script.js
@@ -1,6 +1,5 @@
 var all = prompt("all");
 var names = [];
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','w','x','y','z']; //.split('')
 for (i = 0; i < all; i++) {
     var nameIn = prompt("names");
     names.push("  " + nameIn);
